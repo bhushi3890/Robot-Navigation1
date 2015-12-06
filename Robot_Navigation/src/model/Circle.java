caa67b9e0296db415f458c3df39d03a9f03125c9
Apoorva Jain <felicityapoorva@gmail.com>
2015-12-06 15:46:12 -0500

    Adding circle class

---
+++ Robot_Navigation/src/model/Circle.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Circle extends Coordinates {
+
+	int radius;
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+}
